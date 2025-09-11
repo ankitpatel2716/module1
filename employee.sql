8a878c0093bde5681ec4fdfbbaae70cf502b7daa
rabadiya ankit <rabadiyaa73@gmail.com>
2025-09-12 00:07:59 +0530

    Add files via upload

---
+++ employee.sql
@@ -0,0 +1,58 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Sep 11, 2025 at 09:03 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `employee`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `employee`
+--
+
+CREATE TABLE `employee` (
+  `EMPNO` int(50) NOT NULL,
+  `FIRST_NAME` varchar(60) NOT NULL,
+  `LAST_NAME` varchar(70) NOT NULL,
+  `DESIGNATION` varchar(80) NOT NULL,
+  `HIREDATE` varchar(40) NOT NULL,
+  `SALARY` int(90) NOT NULL,
+  `COMM` int(50) NOT NULL,
+  `DEPTNO` int(70) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `employee`
+--
+
+INSERT INTO `employee` (`EMPNO`, `FIRST_NAME`, `LAST_NAME`, `DESIGNATION`, `HIREDATE`, `SALARY`, `COMM`, `DEPTNO`) VALUES
+(1001, 'STEFAN', 'SALVATORE', 'BUSSINES ANALIST', '17-NOV-97', 50000, 0, 40),
+(1002, 'DIANA', 'LORRENCE', 'TECHNICAL ARCHITECT', '01-MAY-00', 70000, 0, 10),
+(1003, 'JAMES', 'MADIANSAON', 'MANAGER', '19-JUN-88', 88400, 0, 40),
+(1004, 'JONES', 'NICK', 'HR ANALIST', '02-JAN-03', 47000, 0, 30),
+(1005, 'LUCY', 'GELLLER', 'HR ASSOCIATE', '13-JUL-08', 35000, 200, 30),
+(1006, 'ISAAC', 'STEFAN', 'TRAINEE', '13-DEC-15', 20000, 0, 40),
+(1007, 'JOHN', 'SMITH', 'CLERK', '17-DEC-05', 12000, 0, 10),
+(1008, 'NENCY', 'GILLBERT', 'SALESMAN', '20-FEB-81', 18000, 300, 10);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
